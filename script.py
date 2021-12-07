4394658288decbab50cd1ccb3aab02756ebe882d
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2021-12-07 18:27:17 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,22 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}
+
+I'm a Image Editor Bot Of Nidusha Amarasinghe which Supports various modes
+
+For more click help....</b>"""
+    HELP_MSG = """Hai, Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+
+@Nidusha_Bots"""
+    ABOUT_MSG = """⭕️<b>My Name : Image Editor Bot Of Nidusha</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
